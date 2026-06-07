f48ea13e891c106bb408ab78f1e5b55d2bc15596
Aissata Diallo <ad8128975@gmail.com>
2026-06-07 01:45:28 +0000

    Ajout de la page About et des compteurs javaScript

--- js/main.js
+++ js/main.js
@@ -14,3 +14,42 @@ const buttons = document .querySelectorAll('.filter-btn');
         });
     });
  });
+
+ function animatevalue(id ,start,end,end,duration){
+    let obj = ducument.getElementById(id);
+    let range = end -start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTine = Math.abs(Math.floor(duration /range));
+
+    let timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+
+        if (current == end){
+            clearInterval(timer);
+        }
+    },stepTime)
+ }
+ const counters = document.querySelectorAll('counter');
+
+ counters.forEach(counter =>{
+    const target = +counter.dataset.target;
+
+    const  updatecounter = () => {
+        const current = +counter.innerText;
+
+        const increment = target /100;
+
+        if (current < target){
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(updatecounter, 20);
+        } else{
+            counter.innerText = target;
+        }
+    };
+    updatecounter();
+ });
+ animatevalue("freelances",0, 2500, 2000)
+ animatevalue("entreprises",0, 800, 2000)
+ animatevalue("mission",0, 1200, 3000)
